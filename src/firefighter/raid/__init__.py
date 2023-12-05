53f6c93cbccae3313c42977984d6c279b2e1fd78
Gabriel Dugny <gabriel.dugny@manomano.com>
2023-12-05 12:18:02 +0100

    chore: try new CI

--- src/firefighter/raid/__init__.py
+++ src/firefighter/raid/__init__.py
@@ -1,1 +1,6 @@
-from __future__ import annotations
+"""RAID is ManoMano's internal issue dispatcher.
+
+It works with Slack and Jira to create, update and close issues.
+
+It is currently not configurable.
+"""
