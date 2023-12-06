06b14358f4e760779d9b4309958451eaade37807
Gabriel Dugny <gabriel.dugny@manomano.com>
2023-12-06 12:05:21 +0100

    feat: disable Pagerduty, Raid, Confluence by default

--- src/firefighter/firefighter/settings/components/pagerduty.py
+++ src/firefighter/firefighter/settings/components/pagerduty.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 from firefighter.firefighter.settings.components.common import INSTALLED_APPS
 from firefighter.firefighter.settings.settings_utils import config
 
-ENABLE_PAGERDUTY: bool = config("ENABLE_PAGERDUTY", cast=bool)
+ENABLE_PAGERDUTY: bool = config("ENABLE_PAGERDUTY", cast=bool, default=False)
 
 if ENABLE_PAGERDUTY:
     INSTALLED_APPS.append("firefighter.pagerduty")
