c9cc4890c72aa83c0e862ccf1db0d838d49ffe9b
Gabriel Dugny <gabriel.dugny@manomano.com>
2023-12-07 11:49:21 +0100

    chore: move MM specific RAID settings

--- src/firefighter/firefighter/settings/components/raid.py
+++ src/firefighter/firefighter/settings/components/raid.py
@@ -7,19 +7,15 @@ ENABLE_RAID: bool = config("ENABLE_RAID", cast=bool, default=False)
 
 if ENABLE_RAID:
     INSTALLED_APPS += ("firefighter.raid",)
+
+    # Default Jira user ID to use for creating issues
     RAID_DEFAULT_JIRA_QRAFT_USER_ID: str = config("RAID_DEFAULT_JIRA_QRAFT_USER_ID")
-    RAID_DEFAULT_JIRA_ARMATIS_USER_ID: str = config("RAID_DEFAULT_JIRA_ARMATIS_USER_ID")
-    RAID_DEFAULT_JIRA_NEXUS_USER_ID: str = config("RAID_DEFAULT_JIRA_NEXUS_USER_ID")
-    RAID_DEFAULT_JIRA_ADM_USER_ID: str = config("RAID_DEFAULT_JIRA_ADM_USER_ID")
+
+    # The Jira project key to use for creating issues, e.g. "INC"
     RAID_JIRA_PROJECT_KEY: str = config("RAID_JIRA_PROJECT_KEY")
+
+    # Link to the board with issues to qualify
     RAID_QUALIFIER_URL: str = config("RAID_QUALIFIER_URL")
 
-    # XXX OSS: Move to mm_ff_settings
-    RAID_JIRA_USER_IDS: dict[str, str] = {
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-    }
+    # Mapping of domain to default Jira user ID
+    RAID_JIRA_USER_IDS: dict[str, str] = {}
