3a8469af9e0226c6c09dbdd8edfa32e75d39b451
Gabriel Dugny <gabriel.dugny@manomano.com>
2024-05-14 14:38:25 +0200

    chore: move from yarn to npm, fix templates for Daisy UI upgrade (#60)

--- scripts/hatch_build.py
+++ scripts/hatch_build.py
@@ -10,6 +10,6 @@ from hatchling.builders.hooks.plugin.interface import BuildHookInterface
 
 class RollupBuildHook(BuildHookInterface):
     def initialize(self, version: str, build_data: dict[str, Any]) -> Any:
-        subprocess.check_output("yarn install", shell=True)  # noqa: S602,S607
-        subprocess.check_output("yarn run build", shell=True)  # noqa: S602,S607
+        subprocess.check_output("npm install", shell=True)  # noqa: S602,S607
+        subprocess.check_output("npm run build", shell=True)  # noqa: S602,S607
         return super().initialize(version, build_data)
