194e95366a3f02f46b919b1e4d9c873fec7b6368
Gabriel Dugny <gabriel.dugny@manomano.com>
2024-05-14 13:40:08 +0200

    chore: upgrade to django-components 0.74
    - Typings stubs are not longer needed
    - A flag is needed as a default behaviour changed

--- stubs/django_components/__init__.pyi
+++
@@ -1,7 +0,0 @@
-from _typeshed import Incomplete
-from django_components.utils import search as search
-
-default_app_config: str
-
-def autodiscover() -> None: ...
-def import_file(path: Incomplete) -> None: ...
