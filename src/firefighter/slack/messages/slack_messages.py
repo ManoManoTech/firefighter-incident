077b8aa3f90f47fa98a19ffafd9156e8df2c0104
Jenna <jenna.fauconnierdiop@manomano.com>
2024-07-25 11:42:59 +0200

    python CI lint

--- src/firefighter/slack/messages/slack_messages.py
+++ src/firefighter/slack/messages/slack_messages.py
@@ -860,7 +860,9 @@ class SlackMessageChannelReminderPostMortem(SlackMessageSurface):
                     TextObject(
                         type="mrkdwn",
                         text=":bulb: Please take a moment to complete the post-mortem process. "
-                        "It’s essential for continuous improvement!",
+                        """
+                            `It's essential for continuous improvement!`
+                        """,
                     )
                 ]
             ),
