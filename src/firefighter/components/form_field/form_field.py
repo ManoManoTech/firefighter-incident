be2e2cf1e54def6edcbece33cfbc27a9b48fcd16
Gabriel Dugny <gabriel.dugny@manomano.com>
2024-09-30 09:25:17 +0200

    refactor: typed components

--- src/firefighter/components/form_field/form_field.py
+++ src/firefighter/components/form_field/form_field.py
@@ -1,26 +1,34 @@
 from __future__ import annotations
 
 import logging
-from typing import TYPE_CHECKING, Any
+from typing import (
+    Any,
+    Never,
+    TypedDict,
+)
 
-from django_components import component
-
-if TYPE_CHECKING:
-    from django import forms
+from django import forms
+from django_components import EmptyTuple, component
 
 logger = logging.getLogger(__name__)
 
 
+class Data(TypedDict):
+    field: forms.Field | forms.BoundField
+    field_input_class: str
+
+
+class Kwargs(TypedDict, total=True):
+    field: forms.Field | forms.BoundField
+
+
 @component.register("form_field")
-class FormField(component.Component):
+class FormField(component.Component[EmptyTuple, Kwargs, Data, Any]):
     template_name = "form_field/form_field.html"
 
     def get_context_data(
-        self, field: forms.BoundField | forms.Field, *args: Any, **kwargs: Any
-    ) -> dict[str, Any]:
-        if field is None:
-            raise ValueError("Field not set!")
-
+        self, field: forms.BoundField | forms.Field, **kwargs: Never
+    ) -> Data:
         input_class = "input-ff"
 
         return {"field": field, "field_input_class": input_class}
