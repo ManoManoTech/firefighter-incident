de9066a01327a4a4ad0b53ace04a2df369290ffe
Nicolas LAFITTE <nicolas.lafitte@manomano.com>
2025-05-22 17:53:20 +0200

    fix: sla and business impact on create incident

--- src/firefighter/raid/client.py
+++ src/firefighter/raid/client.py
@@ -88,8 +88,8 @@ class RaidJiraClient(JiraClient):
                 f"Suggested team to be routed: *{suggested_team_routing}*"
             )
         if business_impact and business_impact != "N/A":
-            if business_impact not in {"Low", "Medium", "High"}:
-                raise ValueError("Business impact must be Low, Medium or High")
+            if business_impact not in {"Lowest", "Low", "Medium", "High", "Highest"}:
+                raise ValueError("Business impact must be Lowest, Low, Medium, High or Highest")
             extra_args["customfield_10936"] = {"value": business_impact}
         if platform:
             platform = platform.replace("platform-", "")
