196dba36849ce3f980e2178d6dfa9d3841976a7d
Nicolas LAFITTE <nicolas.lafitte@manomano.com>
2026-06-29 16:13:55 +0200

    fix(dust): register generate_dust_postmortem task in Celery task discovery

--- src/firefighter/slack/tasks/__init__.py
+++ src/firefighter/slack/tasks/__init__.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 from firefighter.slack.tasks import (
     fetch_conversations_members,
+    generate_dust_postmortem,
     send_message,
     send_postmortem_reminders,
     send_reminders,
